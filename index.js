9e2cd08a29267313973d5dd334adbabfb40f6ca1
“venky4creator” <venkyv676@gmail.com>
2024-11-11 08:01:54 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const Notification = props => {
+  return (
+    <li claaName={`${props.claaName}`}>
+      <img src={`${props.image}`} alt='image' />
+      <p>{props.message}</p>
+    </li>
+  )
+}
+
+const element = (
+  <div className='notifications-container'>
+    <h1>Notifications</h1>
+    <ul>
+      <Notification
+        className='box1'
+        image='https://assets.ccbp.in/frontend/react-js/primary-icon-img.png'
+        message='Information Message'
+      />
+
+      <Notification
+        className='box2'
+        image='https://assets.ccbp.in/frontend/react-js/success-icon-img.png'
+        message='Success Message'
+      />
+
+      <Notification
+        className='box3'
+        image='https://assets.ccbp.in/frontend/react-js/warning-icon-img.png'
+        message='Warning Message'
+      />
+
+      <Notification
+        className='box4'
+        image='https://assets.ccbp.in/frontend/react-js/danger-icon-img.png'
+        message='Error Message'
+      />
+    </ul>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
